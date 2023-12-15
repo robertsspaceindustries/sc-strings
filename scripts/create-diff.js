469fe1a86f41ca7e0b588307cc8945246f8d881a
Astrid <github@astrid.exposed>
2023-12-16 00:34:36 +0100

    improvements

--- scripts/create-diff.js
+++ scripts/create-diff.js
@@ -5,63 +5,88 @@ import { readFileSync, writeFileSync, existsSync, mkdirSync } from "node:fs";
 existsSync("differences") || mkdirSync("differences");
 
 function generateDiff(oldContent, oldName, newContent, newName) {
-	const differences = diff.diffLines(oldContent, newContent);
+    const differences = diff.diffLines(oldContent, newContent);
 
-	const added = new Set(),
-		removed = new Set();
+    const added = new Set(),
+        removed = new Set();
 
-	for (const difference of differences) {
-		const targetSet = difference.added ? added : difference.removed ? removed : undefined;
-		if (!targetSet) continue;
-		for (const value of difference.value.split("\r\n").filter((v) => v !== ""))
-			targetSet.add((difference.added ? "+" : difference.removed ? "-" : " ") + " " + value); // Added: "+" | Removed: "-" | Neither: " "
-	}
+    for (const difference of differences) {
+        const targetSet = difference.added
+            ? added
+            : difference.removed
+            ? removed
+            : undefined;
+        if (!targetSet) continue;
+        for (const value of difference.value
+            .split("\r\n")
+            .filter((v) => v !== ""))
+            targetSet.add(
+                (difference.added ? "+" : difference.removed ? "-" : " ") +
+                    " " +
+                    value
+            ); // Added: "+" | Removed: "-" | Neither: " "
+    }
 
-	const headerSection = `--- ${oldName}\n` + `+++ ${newName}\n\n`,
-		addedSection = "# Added\n" + [...added].join("\n"),
-		removedSection = "# Removed\n" + [...removed].join("\n");
+    if (added.size === 0 && removed.size === 0) return false;
 
-	return (headerSection || "") + addedSection + "\n\n" + removedSection;
+    const headerSection = `--- ${oldName}\n` + `+++ ${newName}\n\n`,
+        addedSection = "# Added\n" + [...added].join("\n"),
+        removedSection = "# Removed\n" + [...removed].join("\n");
+
+    return (headerSection || "") + addedSection + "\n\n" + removedSection;
 }
 
 const channels = await getLatestBuilds(); // The builds in each channel are antecedent
 
-const baseChannelName = Object.keys(channels).find((channelName) => channels[channelName].base),
-	channelsToCompare = [
-		...Object.keys(channels).map((channel) => [baseChannelName, channel]),
-		...Object.keys(channels)
-			.filter((channel) => channel !== baseChannelName)
-			.map((channel) => [channel, channel]),
-	];
+const baseChannelName = Object.keys(channels).find(
+        (channelName) => channels[channelName].base
+    ),
+    channelsToCompare = [
+        ...Object.keys(channels).map((channel) => [baseChannelName, channel]),
+        ...Object.keys(channels)
+            .filter((channel) => channel !== baseChannelName)
+            .map((channel) => [channel, channel]),
+    ];
 
 console.log("Comparing channels:", channelsToCompare);
 
 for (const [channel1Name, channel2Name] of channelsToCompare) {
-	const channel1 = channels[channel1Name],
-		channel2 = channels[channel2Name];
+    const shouldReverse = channel1Name === channel2Name;
 
-	const channel1Path = "translations/" + channel1Name,
-		channel2Path = "translations/" + channel2Name;
+    let channel1 = channels[channel1Name],
+        channel2 = channels[channel2Name];
 
-	const latest1 = channel1[channel1Name === channel2Name ? 1 : 0], // Old
-		latest2 = channel2[0]; // New
-	if (!(latest1 && latest2)) continue;
-	if (Number(latest1.change) > Number(latest2.change)) continue;
-	
-	console.log(channel1Name, "v.", channel2Name, latest1.name, "old");
-	console.log(channel1Name, "v.", channel2Name, latest2.name, "new");
+    if (shouldReverse)
+        (channel1 = channel1.reverse()), (channel2 = channel2.reverse());
 
-	const removeDuplicates = [...new Set([latest1, latest2])];
-	if (removeDuplicates.length < 2) continue;
+    const channel1Path = "translations/" + channel1Name,
+        channel2Path = "translations/" + channel2Name;
 
-	const diff = generateDiff(
-		readFileSync(channel1Path + "/" + latest1.filename, "utf-8"),
-		latest1.name,
-		readFileSync(channel2Path + "/" + latest2.filename, "utf-8"),
-		latest2.name,
-	);
+    const latest1 = channel1[channel1Name === channel2Name ? 1 : 0], // Old
+        latest2 = channel2[0]; // New
+    if (!(latest1 && latest2)) continue;
 
-	console.log("Made diff for", latest1.name, latest2.name);
+    if (Number(latest1.change) > Number(latest2.change)) continue;
 
-	writeFileSync("differences/" + latest1.stem + " " + latest2.stem + ".diff", diff);
+    console.log(channel1Name, "v.", channel2Name, latest1.name, "old");
+    console.log(channel1Name, "v.", channel2Name, latest2.name, "new");
+
+    const removeDuplicates = [...new Set([latest1, latest2])];
+    if (removeDuplicates.length < 2) continue;
+
+    const diff = generateDiff(
+        readFileSync(channel1Path + "/" + latest1.filename, "utf-8"),
+        latest1.name,
+        readFileSync(channel2Path + "/" + latest2.filename, "utf-8"),
+        latest2.name
+    );
+
+    if (diff) {
+        console.log("Made diff for", latest1.name, latest2.name);
+
+        writeFileSync(
+            "differences/" + latest1.stem + " " + latest2.stem + ".diff",
+            diff
+        );
+    }
 }
--- scripts/utils/get-latest-builds.js
+++ scripts/utils/get-latest-builds.js
@@ -1,59 +1,74 @@
 import fs from "node:fs";
 
 function removeFileExtension(path) {
-	return path
-		.split(".")
-		.slice(0, -1) // Remove file extension element
-		.join(".");
+    return path
+        .split(".")
+        .slice(0, -1) // Remove file extension element
+        .join(".");
 }
 
 function capitalizeFirstLetter(string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function convertFilename(filename, channel) {
-	// eslint-disable-next-line no-unused-vars
-	const [_game, release, version, change] = removeFileExtension(filename).split("-");
+    // eslint-disable-next-line no-unused-vars
+    const [_game, release, version, change] =
+        removeFileExtension(filename).split("-");
 
-	return {
-		release,
-		version,
-		change,
-		name:
-			`Star Citizen ${capitalizeFirstLetter(release)} ` +
-			`${capitalizeFirstLetter(version)} ` +
-			`${channel.toUpperCase()}.${change}`,
-		stem: removeFileExtension(filename) + "-" + channel.replaceAll("-", "_"),
-		filename: filename,
-	};
+    return {
+        release,
+        version,
+        change,
+        name:
+            `Star Citizen ${capitalizeFirstLetter(release)} ` +
+            `${capitalizeFirstLetter(version)} ` +
+            `${channel.toUpperCase()}.${change}`,
+        stem:
+            removeFileExtension(filename) + "-" + channel.replaceAll("-", "_"),
+        filename: filename,
+    };
 }
 
 export function sort(a, b) {
-	const versionComparison = b.version.localeCompare(a.version);
+    const versionComparison = b.version.localeCompare(a.version);
 
-	if (versionComparison === 0) {
-		return b.change - a.change;
-	}
+    if (versionComparison === 0) {
+        return b.change - a.change;
+    }
 
-	return versionComparison;
+    return versionComparison;
 }
 
 export default async function getLatestBuilds() {
-	const channels = fs.readdirSync("translations").filter(
-		(folder) => !fs.existsSync("translations/" + folder + "/.noinclude"), // Remove channels that have the .noinclude file
-	);
+    const channels = fs.readdirSync("translations").filter(
+        (folder) => !fs.existsSync("translations/" + folder + "/.noinclude") // Remove channels that have the .noinclude file
+    );
 
-	const channelsWithBuilds = {};
+    const channelsWithBuilds = {};
 
-	for (const channel of channels) {
-		channelsWithBuilds[channel] = fs
-			.readdirSync("translations/" + channel)
-			.filter((filename) => !filename.startsWith(".") && filename.endsWith(".ini"))
-			.map((filename) => convertFilename(filename, channel))
-			.sort(sort);
+    for (const channel of channels) {
+        channelsWithBuilds[channel] = fs
+            .readdirSync("translations/" + channel)
+            .filter(
+                (filename) =>
+                    !filename.startsWith(".") && filename.endsWith(".ini")
+            )
+            .map((filename) => convertFilename(filename, channel))
+            .sort(sort);
 
-		channelsWithBuilds[channel].base = fs.existsSync("translations/" + channel + "/.base"); // If exists, this channel will be the baseline version for all differences
-	}
+        channelsWithBuilds[channel].base = fs.existsSync(
+            "translations/" + channel + "/.base"
+        ); // If exists, this channel will be the baseline version for all differences
 
-	return channelsWithBuilds;
+        if (fs.existsSync("translations/" + channel + "/.changenum")) {
+            channelsWithBuilds[channel] = channelsWithBuilds[channel].sort(
+                (a, b) => {
+                    return b.change - a.change;
+                }
+            );
+        }
+    }
+
+    return channelsWithBuilds;
 }
